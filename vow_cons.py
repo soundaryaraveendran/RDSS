ec00e6a8f452b38877511a35e9a589ebdce94d2b
soundaryaraveendran <41017433+soundaryaraveendran@users.noreply.github.com>
2018-07-12 14:40:06 +0530

    Update vow_cons.py

--- vow_cons.py
+++ vow_cons.py
@@ -1,6 +1,6 @@
 ip=input()
 if(ip=="a" or ip=="e" or ip=="i" or ip=="o" or ip=="u"):
-	print("Vowels")
+	print("Vowel")
 elif(ip=="!" or ip=="@" or ip=="#" or ip=="$" or ip=="%" or ip=="^" or ip=="&" or ip=="*" or ip=="(" or ip==")" or ip=="_"):
 	print("invalid")
 else:
