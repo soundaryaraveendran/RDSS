b8b4e9ee3e8b7ad3d0bb682dd553819bb711d397
soundaryaraveendran <41017433+soundaryaraveendran@users.noreply.github.com>
2018-07-12 14:36:59 +0530

    Create vow_cons.py

---
+++ vow_cons.py
@@ -0,0 +1,7 @@
+ip=input()
+if(ip=="a" or ip=="e" or ip=="i" or ip=="o" or ip=="u"):
+	print("Vowels")
+elif(ip=="!" or ip=="@" or ip=="#" or ip=="$" or ip=="%" or ip=="^" or ip=="&" or ip=="*" or ip=="(" or ip==")" or ip=="_"):
+	print("invalid")
+else:
+	print("Consonant")
